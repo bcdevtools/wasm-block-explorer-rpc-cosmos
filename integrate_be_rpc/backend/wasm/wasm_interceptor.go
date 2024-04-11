67967b8d0dd8dcaa1663db6c2e59730fe36d1e09
0xbcdev <0xbcdev@bcdev.tools>
2024-04-11 23:21:05 +0700

    intercept account info, return contract code id if is contract, and cw-20 token info if possible

--- integrate_be_rpc/backend/wasm/wasm_interceptor.go
+++ integrate_be_rpc/backend/wasm/wasm_interceptor.go
@@ -3,6 +3,7 @@ package wasm
 import (
 	berpcbackend "github.com/bcdevtools/block-explorer-rpc-cosmos/be_rpc/backend"
 	berpctypes "github.com/bcdevtools/block-explorer-rpc-cosmos/be_rpc/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -73,7 +74,61 @@ func (m *DefaultRequestInterceptor) GetModuleParams(moduleName string) (intercep
 
 // GetAccount returns the contract information if the account is a contract. Other-wise no-op.
 func (m *DefaultRequestInterceptor) GetAccount(accountAddressStr string) (intercepted, append bool, response berpctypes.GenericBackendResponse, err error) {
-	intercepted = false
-	// TODO BE: implement
+	if !m.bech32Cfg.IsAccountAddr(accountAddressStr) {
+		// not an account address, ignore
+		intercepted = false
+		append = false
+		return
+	}
+
+	accAddr, err := sdk.AccAddressFromBech32(accountAddressStr)
+	if err != nil {
+		// not an account address, ignore
+		intercepted = false
+		append = false
+		return
+	}
+
+	if len(accAddr.Bytes()) != 32 {
+		// not a contract address, ignore
+		intercepted = false
+		append = false
+		return
+	}
+
+	intercepted = false // provide information for the account, so we don't need to ignore other response information
+	defer func() {
+		if err == nil {
+			append = true
+		} else {
+			response = nil // eraser
+		}
+	}()
+
+	response = make(berpctypes.GenericBackendResponse)
+
+	codeId, err := m.backend.GetContractCodeId(accountAddressStr)
+	if err != nil {
+		err = status.Error(codes.Internal, errors.Wrap(err, "failed to check contract code id").Error())
+		return
+	}
+
+	if codeId == 0 {
+		// not a contract, ignore
+		return
+	}
+
+	contractInfo := berpctypes.GenericBackendResponse{
+		"codeId": codeId,
+	}
+	response["contract"] = contractInfo
+
+	cw20TokenInfo, err := m.backend.GetCw20ContractInfo(accountAddressStr)
+	if err == nil && len(cw20TokenInfo) > 0 {
+		for k, v := range cw20TokenInfo {
+			contractInfo[k] = v
+		}
+	}
+
 	return
 }
